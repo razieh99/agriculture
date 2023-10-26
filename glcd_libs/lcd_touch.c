050948b95c9f378488c5689ac7274b2efb607d48
Razieh Sia <82574479+razieh99@users.noreply.github.com>
2023-10-26 12:43:09 +0330

    Add files via upload

---
+++ glcd_libs/lcd_touch.c
@@ -0,0 +1,137 @@
+/*
+ * lcd_touch.c
+ *
+ *  Created on: Oct 29, 2016
+ *      Author: Danylo Ulianych
+ *
+ *
+ *
+ */
+
+#include <stdlib.h>
+#include "lcd_touch.h"
+#include "glcdsia.h"
+#include "delay.h"
+#include "stdio.h"
+
+#define TOUCH_ADC_X_MAX 940
+#define TOUCH_ADC_X_MIN 140
+#define TOUCH_ADC_Y_MIN 140
+#define TOUCH_ADC_Y_MAX 1000
+
+
+// Voltage Reference: AVCC pin
+#define ADC_VREF_TYPE ((0<<REFS1) | (1<<REFS0) | (0<<ADLAR))
+
+void ADC_init(){
+
+// ADC initialization
+// ADC Clock frequency: 125.000 kHz
+// ADC Voltage Reference: AVCC pin
+ADMUX=ADC_VREF_TYPE;
+ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADFR) | (0<<ADIF) | (0<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (0<<ADPS0);
+SFIOR=(0<<ACME);
+}
+static uint32_t touchX() {
+    /*
+     * set the touch pins like this :
+     *_yp ---------->  INPUT(ADC)   ------->  PA1
+     *_ym ---------->  INPUT(LOW)   ------->  PB1
+     *_xp ---------->  OUTPUT(HIGH) ------->  PB0
+     *_xm ---------->  OUTPUT(LOW)  ------->  PA2
+     */       
+                     DDRA.4 = 0;   //set as input    D1
+                     PORTA.4 = 0;  // SET AS LOW
+                     DDRA.3 = 1;   // set as output   D0
+                     PORTA.3 = 1 ; // set as high
+//                     DDRA.6 = 1;   // set as output   RS
+//                     PORTA.6 = 0 ; // set as low    
+//                     DDRA.5 = 0;   //set as input
+//                     PORTA.5 = 0;    // set as low      CS 
+                     DDRF=(1<<DDF6) | (0<<DDF5) ;
+                     PORTF= (0<<PORTF6) | (0<<PORTF5) ;
+                                           
+
+        return  read_adc(5);
+}
+static uint32_t touchY() {
+      /*
+       * set the touch pins like this :
+       *_yp ---------> OUTPUT(HIGH) ------->  PA1
+       *_ym --------->  OUTPUT(LOW) ------->  PB1
+       *_xp ---------> INPUT(LOW)   ------->  PB0
+       *_xm --------->  INPUT(ADC)  ------->  PA2
+       */
+       // coordinate y :
+                     DDRA.3 = 0;   //set as input
+                     PORTA.3 = 0;  // SET AS LOW
+                     //DDRF.5 = 1;   // set as output
+                     //PORTF.5 = 1 ; // set as high
+                     DDRA.4 = 1;   // set as output
+                     PORTA.4 = 0 ; // set as low 
+                     //DDRF.6 = 0;   //set as input 
+                     //PORTF.6= 0;    // set as low 
+                     DDRF=(0<<DDF6) | (1<<DDF5) ;
+                     PORTF= (0<<PORTF6) | (1<<PORTF5) ;
+                                       
+
+        return read_adc(6);
+
+}
+
+
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+/*
+ * Reads raw touch x- and y-positions and draw on the lcd
+ */ 
+ 
+int LCD_Touch_Read() {
+    uint32_t  x ,y;
+    uint16_t x1,y1;  
+    int i,back=0;
+   // char str1[20],str2[20];
+    ADC_init();
+    x = 0;  
+    i = 0;
+    for (i=0;i<=10;i++){
+    x = x+touchX()/10; 
+    delay_us(1);
+    }
+    i = 0; 
+    y = 0;  
+     for (i=0;i<=10;i++){
+    y = y+touchY()/10; 
+    delay_us(1);
+    }     
+    // scale ADC range to tft height
+     x1 = map(x,TOUCH_ADC_X_MIN, TOUCH_ADC_X_MAX,240,0 );
+    
+    // scale ADC range to tft weight
+     y1 = map(y,TOUCH_ADC_Y_MIN,TOUCH_ADC_Y_MAX, 320,0);
+    // set pins to draw 
+                  DDRF=(1<<DDF6) | (1<<DDF5) ;
+                   DDRA.3 = 1;
+                   DDRA.4 = 1;  
+                   //glcdsia_putpixel(x1,y1,GLCD_CL_WHITE);
+      // setting touch
+   if (180<x1 && x1<230) 
+       if (5<y1 && y1<35) {
+                             back = 1;
+       }
+                    // back touch
+    if (5<x1 && x1<60) 
+        if (270<y1 && y1<320) {
+                                back = 2;
+        } 
+//        if (80<x1 && x1<160) 
+//        if (100<y1 && y1<250) {
+//                                back = 4;
+//        } 
+        if (back != 1 && back != 2) back =0; // no touch
+return back;
+}
+
+
